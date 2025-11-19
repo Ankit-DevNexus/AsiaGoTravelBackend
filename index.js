59e8b43266e473f0463a1370cc505a159615bee2
Abdul Azeem <abdulazeem@devnexus.in>
2025-11-19 12:07:14 +0530

    change in index,js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import express from "express";
 
-import cors from 'cors';
+import cors from "cors";
 import mongoose from "mongoose";
 import Routes from "./Routes/routes.js";
 import bodyParser from "body-parser";
@@ -18,24 +18,25 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
-app.use('/api', Routes);
+app.use("/api", Routes);
 
 // for testing
 app.get("/", (req, res) => {
-    res.send("AsiaGoTravel");
+  res.send("AsiaGoTravel");
 });
 
 // mongodb connection
 
-mongoose.connect(uri)
-    .then(() => {
-        console.log("✅ DB Connected Successfully");
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on port ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error("❌ MongoDB Connection Failed:", err.message);
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("✅ DB Connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB Connection Failed:", err.message);
+  });
