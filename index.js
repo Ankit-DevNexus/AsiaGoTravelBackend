fd417bc10aba4ab9dfb77a856380f6780cb1d5dd
abdulDevnexus9634 <abdulazeem@devnexus.in>
2025-12-05 17:45:08 +0530

    ..

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from "express";
 
 import cors from "cors";
 import mongoose from "mongoose";
-import Routes from "./Routes/routes.js";
+import Routes from "./routes/routes.js";
 import bodyParser from "body-parser";
 
 const PORT = process.env.PORT || 3030;
